65935e204ed29637cb31181b07b472af634462b4
ohana26 <dvirwebdevelopment.@gmail.com>
2023-12-28 12:06:51 -0500

    add tests

---
+++ src/components/Table.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import axios from 'axios';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Table } from './Table';
+
+jest.mock('../types/users', () => ({
+  User: jest.fn(),
+}));
+
+const testData = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    phone: '123-456-7890',
+  },
+];
+
+describe('Table component', () => {
+  it('renders table with data and pagination', () => {
+    render(<Table data={testData} />);
+
+    expect(screen.getByText('ID')).toBeInTheDocument();
+    expect(screen.getByText('First Name')).toBeInTheDocument();
+    expect(screen.getByText('Last Name')).toBeInTheDocument();
+    expect(screen.getByText('Email')).toBeInTheDocument();
+    expect(screen.getByText('Phone')).toBeInTheDocument();
+
+    expect(screen.getByText('John')).toBeInTheDocument();
+    expect(screen.getByText('Doe')).toBeInTheDocument();
+    expect(screen.getByText('john.doe@example.com')).toBeInTheDocument();
+    expect(screen.getByText('123-456-7890')).toBeInTheDocument();
+    expect(screen.getByText('Prev')).toBeInTheDocument();
+    expect(screen.getByText('Next')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Next'));
+
+  });
+
+});
