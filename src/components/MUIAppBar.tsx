485d7be33bb9978dc56fb452b889023be3bb74fd
ohana26 <dvirwebdevelopment.@gmail.com>
2023-12-28 12:19:11 -0500

    fix bar

--- src/components/MUIAppBar.tsx
+++ src/components/MUIAppBar.tsx
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import { useLocation } from 'react-router-dom';
 import { Route as pageRoute } from '../types/route';
 
@@ -18,14 +17,6 @@ export const MUIAppBar: React.FC = () => {
   return (<Box sx={{ flexGrow: 1 }}>
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           {routeName}
         </Typography>
